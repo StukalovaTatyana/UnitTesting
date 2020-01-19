6a4335cdc7c6e547d57431e1c954d2407ad9e718
Tatyana <tatyana-stukalova@mail.ru>
2020-01-19 22:52:32 +0400

    Реализация вычитания

--- src/main/java/ru/asu/types/MyInt.java
+++ src/main/java/ru/asu/types/MyInt.java
@@ -8,70 +8,59 @@ public class MyInt {
     private int sign;
 
     public MyInt(int value) {
+        this.valueString = String.valueOf(value);
         if (value < 0) {
             this.sign = 1;
+            this.valueString = this.getValueString().substring(1, this.valueString.length());
         } else {
             this.sign = 0;
         }
-
-        this.valueString = String.valueOf(value);
     }
 
     public MyInt(String value) {
+        this.valueString = value;
         if (value.charAt(0) == '-') {
             this.sign = 1;
+            this.valueString = this.getValueString().substring(1, this.valueString.length());
         } else {
             this.sign = 0;
         }
-        this.valueString = value;
     }
 
     public MyInt(byte[] value) {
+        this.valueString = String.valueOf(value);
         if (((char) value[0]) == '-') {
             this.sign = 1;
+            this.valueString = this.getValueString().substring(1, this.valueString.length());
         } else {
             this.sign = 0;
         }
-        this.valueString = String.valueOf(value);
     }
 
     public MyInt add(MyInt value) {
-        StringBuilder result = new StringBuilder();
-
-        int razn = this.valueString.length() - value.valueString.length();
-        this.valueString = "0" + this.valueString;
-        value.valueString = "0" + value.valueString;
-        if (razn > 0) {
-            value.addZero(razn);
-        } else if (razn < 0) {
-            this.addZero(razn);
+        if (this.sign == 0 && value.sign == 0) {
+            return this.sum(value);
+        } else if (this.sign == 0 && value.sign == 1) {
+            return this.minus(value);
+        } else if (this.sign == 1 && value.sign == 0) {
+            return value.minus(this);
+        } else {
+            MyInt sum = this.sum(value);
+            sum.sign = sum.sign == 0 ? 1 : 0;
+            return sum;
         }
-
-        this.reverse();
-        value.reverse();
-
-        int shift = 0;
-        for (int i = 0; i < this.valueString.length(); i++) {
-            int a = Character.getNumericValue(this.valueString.charAt(i));
-            int b = Character.getNumericValue(value.valueString.charAt(i));
-
-            int res = a + b + shift;
-
-            result.append(res % 10);
-            shift = res / 10;
-        }
-        result.reverse();
-        if (result.charAt(0) == '0') {
-            result.deleteCharAt(0);
-        }
-
-
-        MyInt r = new MyInt(result.toString());
-        return r;
     }
 
     public MyInt subtract(MyInt value) {
-        return null;
+        if (this.sign == 0 && value.sign == 0) {
+            return this.minus(value);
+        } else if (this.sign == 0 && value.sign == 1) {
+            return null;
+        } else if (this.sign == 1 && value.sign == 0) {
+            return null;
+        } else {
+            return null;
+        }
     }
 
     public MyInt multiply(MyInt value) {
@@ -104,6 +93,115 @@ public class MyInt {
 
     public MyInt longValue(MyInt value) {
         return null;
+    }
+
+    private MyInt sum(MyInt value) {
+        StringBuilder result = new StringBuilder();
+
+        int razn = this.valueString.length() - value.valueString.length();
+        this.valueString = "0" + this.valueString;
+        value.valueString = "0" + value.valueString;
+        if (razn > 0) {
+            value.addZero(razn);
+        } else if (razn < 0) {
+            this.addZero(razn);
+        }
+
+        this.reverse();
+        value.reverse();
+
+        int shift = 0;
+        for (int i = 0; i < this.valueString.length(); i++) {
+            int a = Character.getNumericValue(this.valueString.charAt(i));
+            int b = Character.getNumericValue(value.valueString.charAt(i));
+
+            int res = a + b + shift;
+
+            result.append(res % 10);
+            shift = res / 10;
+        }
+        result.reverse();
+        if (result.charAt(0) == '0') {
+            result.deleteCharAt(0);
+        }
+
+        MyInt r = new MyInt(result.toString());
+        return r;
+    }
+
+    private MyInt minus(MyInt value) {
+        StringBuilder result = new StringBuilder();
+        int razn = this.valueString.length() - value.valueString.length();
+        int flag = 0;
+        if (razn > 0) {
+            flag = 0;
+        } else if (razn < 0) {
+            flag = 1;
+        } else {
+            for (int i = 0; i < this.valueString.length(); i++) {
+                int a = Character.getNumericValue(this.valueString.charAt(i));
+                int b = Character.getNumericValue(value.valueString.charAt(i));
+                if (a > b) {
+                    flag = 0;
+                    break;
+                } else if (a < b) {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+
+        if (flag == 1) {
+            this.addZero(razn);
+            this.reverse();
+            value.reverse();
+
+            minus(value.valueString, this.valueString, result);
+        } else {
+            value.addZero(razn);
+            this.reverse();
+            value.reverse();
+
+            minus(this.valueString, value.valueString, result);
+        }
+
+        int resultSign;
+        if (flag == 0) {
+            resultSign = this.sign;
+        } else if (this.sign == value.sign) {
+            resultSign = Math.abs(this.sign - 1);
+        } else {
+            resultSign = value.sign;
+        }
+
+        result.reverse();
+
+        if (result.charAt(0) == '0') {
+            result.deleteCharAt(0);
+        }
+
+        MyInt r = new MyInt(result.toString());
+        if (resultSign == 1 && !String.valueOf(result).equals("0")) {
+            r.sign = 1;
+        }
+        return r;
+    }
+
+    private void minus(String first, String second, StringBuilder result) {
+        int adder = 10;
+        int regulator = 0;
+        for (int i = 0; i < first.length(); i++) {
+            int a = Character.getNumericValue(first.charAt(i));
+            int b = Character.getNumericValue(second.charAt(i));
+
+            int x = a + adder - regulator - b;
+            regulator = 1;
+            if (x >= adder) {
+                x -= adder;
+                regulator = 0;
+            }
+            result.append(x);
+        }
     }
 
     public String getValueString() {
--- src/test/java/types/MyIntTest.java
+++ src/test/java/types/MyIntTest.java
@@ -3,6 +3,8 @@ package types;
 import org.junit.Test;
 import ru.asu.types.MyInt;
 
+import java.text.MessageFormat;
+
 import static org.junit.Assert.assertEquals;
 
 public class MyIntTest {
@@ -29,13 +31,17 @@ public class MyIntTest {
     @Test
     public void signStringTest() {
         MyInt a = new MyInt("123");
-        assertEquals("Значения не совпали", 0, a.getSign());
+        assertEquals("Знаки не совпали", 0, a.getSign());
+        assertEquals("Значения не совпали", "123", a.getValueString());
 
         a = new MyInt("-13");
-        assertEquals("Значения не совпали", 1, a.getSign());
+        assertEquals("Знаки не совпали", 1, a.getSign());
+        assertEquals("Значения не совпали", "13", a.getValueString());
 
         a = new MyInt("0");
-        assertEquals("Значения не совпали", 0, a.getSign());
+        assertEquals("Знаки не совпали", 0, a.getSign());
+        assertEquals("Значения не совпали", "0", a.getValueString());
+
     }
 
     @Test
@@ -98,39 +104,97 @@ public class MyIntTest {
 
     @Test
     public void addPositiveToNegativeTest() {
-        MyInt a = new MyInt(123);
+        MyInt a = new MyInt(124);
         MyInt b = new MyInt(-3);
-
-        MyInt c = new MyInt(120);
+        MyInt c = new MyInt(121);
         MyInt actual = a.add(b);
-        assertEquals("Значения не совпали", c, actual);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
 
         a = new MyInt(89);
         b = new MyInt(-11);
         c = new MyInt("78");
         actual = a.add(b);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
 
-        assertEquals("Значения не совпали", c, actual);
-
-        a = new MyInt(2);
-        b = new MyInt(-3);
-        c = new MyInt("-5");
+        a = new MyInt(222);
+        b = new MyInt(-333);
+        c = new MyInt("-111");
         actual = a.add(b);
-        assertEquals("Значения не совпали", c, actual);
-
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
 
         a = new MyInt(3);
         b = new MyInt(-123);
         c = new MyInt("-120");
         actual = a.add(b);
-        assertEquals("Значения не совпали", c, actual);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
 
-        a = new MyInt(10);
-        b = new MyInt(-1);
+        a = new MyInt(11);
+        b = new MyInt(-2);
         c = new MyInt("9");
         actual = a.add(b);
-        assertEquals("Значения не совпали", c, actual);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
     }
 
+    @Test
+    public void addNegativeToPositive() {
+        MyInt a = new MyInt(-11);
+        MyInt b = new MyInt(2);
+        MyInt c = new MyInt("-9");
+        MyInt actual = a.add(b);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
+
+        a = new MyInt(-13);
+        b = new MyInt(2);
+        c = new MyInt("-11");
+        actual = a.add(b);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
+
+        a = new MyInt(-23);
+        b = new MyInt(11);
+        c = new MyInt("-12");
+        actual = a.add(b);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
+
+        a = new MyInt(-23);
+        b = new MyInt(31);
+        c = new MyInt("8");
+        actual = a.add(b);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
+    }
+
+    @Test
+    public void addNegativeToNegative() {
+        MyInt a = new MyInt(-11);
+        MyInt b = new MyInt(-2);
+        MyInt c = new MyInt("-13");
+        MyInt actual = a.add(b);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
+
+        a = new MyInt(-91);
+        b = new MyInt(-9);
+        c = new MyInt("-100");
+        actual = a.add(b);
+        assertEquals(MessageFormat.format("Значения не совпали! Ожидалось:{0}, пришло:{1}", c.getValueString(), actual.getValueString()), c, actual);
+    }
+
+    @Test
+    public void subtractPositiveToPositiveTest() {
+
+    }
+
+    @Test
+    public void subtractPositiveToNegativeTest() {
+
+    }
+
+    @Test
+    public void subtractNegativeToPositiveTest() {
+
+    }
+
+    @Test
+    public void subtractNegativeToNegativeTest() {
+
+    }
 
 }
